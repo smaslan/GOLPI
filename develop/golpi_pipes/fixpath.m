8fed9c27f7ba15836f3e11aff8f331de8ee7d55d
smaslan-116 <smaslan@cmi.cz>
2025-09-01 16:11:00 +0200

    improved PATH env variable fix for Octave pkg install

--- develop/golpi_pipes/fixpath.m
+++ develop/golpi_pipes/fixpath.m
@@ -1,5 +1,5 @@
 % reorder PATH entries so Octave...usr/bin is on top (needed to make pkg install work!)
 _paths = unique(strsplit(getenv("PATH"),';'),'stable');
-_usrlist = cellfun(@isempty,regexp(_paths,'[Oo]ctave.+\\usr\\bin|[Oo]ctave.+\\bin'));
-setenv("PATH",[strcat(_paths([find(~_usrlist) find(_usrlist)]),';'){:}]);
-clear _paths _usrlist;
+[~,_pathids] = sort(cellfun(@isempty,regexp(_paths,'[Oo]ctave.+\\usr\\bin|[Oo]ctave.+\\bin')));
+setenv("PATH",strjoin(_paths(_pathids),';'));
+clear _paths _pathids;
